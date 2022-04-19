c52474f7b51ab53b68b191845ede1e67f27781b8
Igor <igorpigidin@gmail.com>
2022-04-19 13:14:28 +1000

    сделал модальное окно присоединения к игре

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 let modalWindowCreateNewGame = document.querySelector(".modal-window_create-new-game")
 let btnCreateNewGame = document.querySelector(".button_create-game")
-let btnCloseModalWindow = document.querySelector(".modal-window_close")
+let btnCloseNewGameModalWindow = document.querySelector(".modal-window-creating_close")
 
-btnCreateNewGame.addEventListener('click', openModalWindow)
-btnCloseModalWindow.addEventListener('click', closeModalWindow)
+let btnJoinModalWindow = document.querySelector(".button_join-game")
+let modalwindowJoinToGame = document.querySelector(".modal-window_join-to-game")
+let btnCloseJoinModalWindow = document.querySelector(".modal-window-join_close")
 
-function openModalWindow() {
+btnCreateNewGame.addEventListener('click', openNewGameModalWindow)
+btnCloseNewGameModalWindow.addEventListener('click', closeNewGameModalWindow)
+
+btnJoinModalWindow.addEventListener('click', openJoinModalWindow)
+btnCloseJoinModalWindow.addEventListener('click', closeJoinModalWindow)
+
+
+function openNewGameModalWindow() {
   modalWindowCreateNewGame.style.display = "inline"
 }
 
-function closeModalWindow() {
+function closeNewGameModalWindow() {
   modalWindowCreateNewGame.style.display = "none"
+}
+
+function openJoinModalWindow() {
+  modalwindowJoinToGame.style.display = "inline"
+}
+
+function closeJoinModalWindow() {
+  modalwindowJoinToGame.style.display = "none"
 }
