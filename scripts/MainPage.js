54cc2b259cc311dbd14f5c31b1a5a27d77df3b90
Igor <igorpigidin@gmail.com>
2022-04-21 00:28:26 +1000

    сделал взаимодействие страниц и модальных окон

--- scripts/MainPage.js
+++ scripts/MainPage.js
@@ -7,12 +7,18 @@ let modalwindowJoinToGame = document.querySelector(".modal-window_join-to-game")
 let btnCloseJoinModalWindow = document.querySelector(".modal-window-join_close")
 
 let btnlogIn = document.querySelector(".log_in")
+let btnOpenLoginMWFromAuth = document.querySelector(".sign-in_log-in")
 let modalWindowReg = document.querySelector(".modal-window_log-in")
 let btnCloseRegModalWindow = document.querySelector(".log-in_close")
+let btnEndRegistration = document.querySelector(".log-in_registration")
 
 let btnOpenAuthorizationMW = document.querySelector(".sign_in")
+let btnOpenAuthFromLogin = document.querySelector(".log-in_sign-in")
+let btnOpenAuthFromCreateNewGameMW = document.querySelector(".modal-window-creating_sign-in")
+let btnOpenAuthFromJoin = document.querySelector(".modal-window-join_sign-in")
 let AuthorizationMW = document.querySelector(".modal-window_sign-in")
 let btnCloseAuthorizationMW = document.querySelector(".sign-in_close")
+let btnEndAuth = document.querySelector(".sign-in_continue")
 
 btnCreateNewGame.addEventListener('click', openNewGameModalWindow)
 btnCloseNewGameModalWindow.addEventListener('click', closeModalWindows)
@@ -21,28 +27,38 @@ btnJoinModalWindow.addEventListener('click', openJoinModalWindow)
 btnCloseJoinModalWindow.addEventListener('click', closeModalWindows)
 
 btnlogIn.addEventListener('click', openRegModalWindow)
+btnOpenLoginMWFromAuth.addEventListener('click', openRegModalWindow)
 btnCloseRegModalWindow.addEventListener('click', closeModalWindows)
+btnEndRegistration.addEventListener('click', closeModalWindows)
 
 btnOpenAuthorizationMW.addEventListener('click', openAuthModalWindow)
+btnOpenAuthFromLogin.addEventListener('click', openAuthModalWindow)
+btnOpenAuthFromCreateNewGameMW.addEventListener('click', openAuthModalWindow)
+btnOpenAuthFromJoin.addEventListener('click', openAuthModalWindow)
 btnCloseAuthorizationMW.addEventListener('click', closeModalWindows)
+btnEndAuth.addEventListener('click', closeModalWindows)
 
 // модалка для создания новой игры
 function openNewGameModalWindow() {
+  closeModalWindows()
   modalWindowCreateNewGame.style.display = "inline"
 }
 
 // модалка для присоединения к существующей игре
 function openJoinModalWindow() {
+  closeModalWindows()
   modalwindowJoinToGame.style.display = "inline"
 }
 
 // модалка для регистрации
 function openRegModalWindow() {
+  closeModalWindows()
   modalWindowReg.style.display = "inline"
 }
 
 // модалка для авторизации
 function openAuthModalWindow() {
+  closeModalWindows()
   AuthorizationMW.style.display = "inline"
 }
 
